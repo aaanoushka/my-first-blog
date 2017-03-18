ccb286cb594f689e286b0edf21f9f6a0c0d76374
Anna roy <aaanoushka@gmail.com>
2017-03-18 14:23:19 +0100

    change allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3^krs(iv*myh-mbtkmu7a(o^q1)yj-^lcg==3&mzs7kx_&e6oe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aaanoushka.pythonanywhere.com']
 
 
 # Application definition
